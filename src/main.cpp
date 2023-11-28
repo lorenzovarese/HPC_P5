a935713026e820a50bd6fa0e85d971303655a5ab
LorenzoVarese <lorenzo.varese@usi.ch>
2023-11-28 13:27:36 +0100

    Initialize and finalize MPI

--- src/main.cpp
+++ src/main.cpp
@@ -46,37 +46,37 @@ void write_binary(std::string fname, Field &u, SubDomain &domain, Discretization
             fname.c_str(),
             MPI_MODE_CREATE | MPI_MODE_WRONLY,
             MPI_INFO_NULL,
-            &filehandle
-        );
-    assert(result==MPI_SUCCESS);
+            &filehandle);
+    assert(result == MPI_SUCCESS);
 
-    int ustart[]  = {domain.startx-1, domain.starty-1};
-    int ucount[]  = {domain.nx, domain.ny};
+    int ustart[] = {domain.startx - 1, domain.starty - 1};
+    int ucount[] = {domain.nx, domain.ny};
     int dimuids[] = {options.nx, options.ny};
 
     result = MPI_Type_create_subarray(2, dimuids, ucount, ustart, MPI_ORDER_FORTRAN, MPI_DOUBLE, &filetype);
-    assert(result==MPI_SUCCESS);
+    assert(result == MPI_SUCCESS);
 
     result = MPI_Type_commit(&filetype);
-    assert(result==MPI_SUCCESS);
+    assert(result == MPI_SUCCESS);
 
     result = MPI_File_set_view(filehandle, disp, MPI_DOUBLE, filetype, "native", MPI_INFO_NULL);
-    assert(result==MPI_SUCCESS);
+    assert(result == MPI_SUCCESS);
 
     result = MPI_File_write_all(filehandle, u.data(), domain.N, MPI_DOUBLE, MPI_STATUS_IGNORE);
-    assert(result==MPI_SUCCESS);
+    assert(result == MPI_SUCCESS);
 
     result = MPI_Type_free(&filetype);
-    assert(result==MPI_SUCCESS);
+    assert(result == MPI_SUCCESS);
 
     result = MPI_File_close(&filehandle);
-    assert(result==MPI_SUCCESS);
+    assert(result == MPI_SUCCESS);
 }
 
 // read command line arguments
-static void readcmdline(Discretization& options, int argc, char* argv[])
+static void readcmdline(Discretization &options, int argc, char *argv[])
 {
-    if (argc<5 || argc>6 ) {
+    if (argc < 5 || argc > 6)
+    {
         std::cerr << "Usage: main nx ny nt t\n";
         std::cerr << "  nx  number of gridpoints in x-direction\n";
         std::cerr << "  ny  number of gridpoints in y-direction\n";
@@ -88,35 +88,40 @@ static void readcmdline(Discretization& options, int argc, char* argv[])
 
     // read nx
     options.nx = atoi(argv[1]);
-    if (options.nx < 1) {
+    if (options.nx < 1)
+    {
         std::cerr << "nx must be positive integer\n";
         exit(-1);
     }
 
     // read ny
     options.ny = atoi(argv[2]);
-    if (options.ny < 1) {
+    if (options.ny < 1)
+    {
         std::cerr << "ny must be positive integer\n";
         exit(-1);
     }
 
     // read nt
     options.nt = atoi(argv[3]);
-    if (options.nt < 1) {
+    if (options.nt < 1)
+    {
         std::cerr << "nt must be positive integer\n";
         exit(-1);
     }
 
     // read total time
     double t = atof(argv[4]);
-    if (t < 0) {
+    if (t < 0)
+    {
         std::cerr << "t must be positive real value\n";
         exit(-1);
     }
 
     verbose_output = false;
-    if( argc==6 ) {
-        verbose_output = (domain.rank==0);
+    if (argc == 6)
+    {
+        verbose_output = (domain.rank == 0);
     }
 
     // compute timestep size
@@ -132,16 +137,19 @@ static void readcmdline(Discretization& options, int argc, char* argv[])
 
 // ==============================================================================
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     // read command line arguments
     readcmdline(options, argc, argv);
 
-    //TODO:  initialize MPI
+    // initialize MPI
+    MPI_Init(&argc, &argv);
     int thread_level;
 
-    //TODO: get rank and the number of ranks
+    // get rank and the number of ranks
     int mpi_rank, mpi_size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
 
     // initialize subdomain
     domain.init(mpi_rank, mpi_size, options);
@@ -149,40 +157,44 @@ int main(int argc, char* argv[])
 
     int nx = domain.nx;
     int ny = domain.ny;
-    int N  = domain.N;
-    int nt  = options.nt;
+    int N = domain.N;
+    int nt = options.nt;
 
     // set iteration parameters
-    int max_cg_iters     = 200;
+    int max_cg_iters = 200;
     int max_newton_iters = 50;
-    double tolerance     = 1.e-6;
+    double tolerance = 1.e-6;
 
-    if( domain.rank == 0 ) {
+    if (domain.rank == 0)
+    {
         std::cout << "========================================================================" << std::endl;
         std::cout << "                      Welcome to mini-stencil!" << std::endl;
         std::cout << "version   :: OpenMP with MPI : " << domain.size << " MPI ranks" << std::endl;
         std::cout << "mesh      :: " << options.nx << " * " << options.ny << " dx = " << options.dx << std::endl;
-        std::cout << "time      :: " << nt << " time steps from 0 .. " << options.nt*options.dt << std::endl;;
-        std::cout << "iteration :: " << "CG "          << max_cg_iters
-                                     << ", Newton "    << max_newton_iters
-                                     << ", tolerance " << tolerance << std::endl;;
+        std::cout << "time      :: " << nt << " time steps from 0 .. " << options.nt * options.dt << std::endl;
+        ;
+        std::cout << "iteration :: "
+                  << "CG " << max_cg_iters
+                  << ", Newton " << max_newton_iters
+                  << ", tolerance " << tolerance << std::endl;
+        ;
         std::cout << "========================================================================" << std::endl;
     }
 
     // allocate global fields
-    x_new.init(nx,ny);
-    x_old.init(nx,ny);
-    bndN.init(nx,1);
-    bndS.init(nx,1);
-    bndE.init(ny,1);
-    bndW.init(ny,1);
-    buffN.init(nx,1);
-    buffS.init(nx,1);
-    buffE.init(ny,1);
-    buffW.init(ny,1);
+    x_new.init(nx, ny);
+    x_old.init(nx, ny);
+    bndN.init(nx, 1);
+    bndS.init(nx, 1);
+    bndE.init(ny, 1);
+    bndW.init(ny, 1);
+    buffN.init(nx, 1);
+    buffS.init(nx, 1);
+    buffE.init(ny, 1);
+    buffW.init(ny, 1);
 
-    Field b(nx,ny);
-    Field deltax(nx,ny);
+    Field b(nx, ny);
+    Field deltax(nx, ny);
 
     // set dirichlet boundary conditions to 0 all around
     ss_fill(bndN, 0.);
@@ -197,14 +209,14 @@ int main(int argc, char* argv[])
     double xc = 1.0 / 4.0;
     double yc = (options.ny - 1) * options.dx / 4;
     double radius = std::min(xc, yc) / 2.0;
-    for (int j = domain.starty-1; j < domain.endy; j++)
+    for (int j = domain.starty - 1; j < domain.endy; j++)
     {
         double y = (j - 1) * options.dx;
-        for (int i = domain.startx-1; i < domain.endx; i++)
+        for (int i = domain.startx - 1; i < domain.endx; i++)
         {
             double x = (i - 1) * options.dx;
             if ((x - xc) * (x - xc) + (y - yc) * (y - yc) < radius * radius)
-                x_new(i-domain.startx+1, j-domain.starty+1) = 0.1;
+                x_new(i - domain.startx + 1, j - domain.starty + 1) = 0.1;
         }
     }
 
@@ -226,7 +238,7 @@ int main(int argc, char* argv[])
         double residual;
         bool converged = false;
         int it;
-        for (it=0; it<max_newton_iters; it++)
+        for (it = 0; it < max_newton_iters; it++)
         {
             // compute residual : requires both x_new and x_old
             diffusion(x_new, b);
@@ -244,29 +256,33 @@ int main(int argc, char* argv[])
             ss_cg(deltax, b, max_cg_iters, tolerance, cg_converged);
 
             // check that the CG solver converged
-            if (!cg_converged) break;
+            if (!cg_converged)
+                break;
 
             // update solution
             ss_axpy(x_new, -1.0, deltax);
         }
-        iters_newton += it+1;
+        iters_newton += it + 1;
 
-        #ifdef OUTPUT_EVERY_STEP
+#ifdef OUTPUT_EVERY_STEP
         std::stringstream str;
-        str << "output" << timestep << ".bin"; // get filename for this time step
+        str << "output" << timestep << ".bin";           // get filename for this time step
         write_binary(str.str(), x_old, domain, options); // write binary solution to file
-        #endif
+#endif
 
         // output some statistics
-        if (converged && verbose_output) {
+        if (converged && verbose_output)
+        {
             std::cout << "step " << timestep
                       << " required " << it
                       << " iterations for residual " << residual
                       << std::endl;
         }
-        if (!converged) {
+        if (!converged)
+        {
             std::cerr << "step " << timestep
-                      << " ERROR : nonlinear iterations failed to converge" << std::endl;;
+                      << " ERROR : nonlinear iterations failed to converge" << std::endl;
+            ;
             break;
         }
     }
@@ -283,35 +299,38 @@ int main(int argc, char* argv[])
     write_binary("output.bin", x_old, domain, options);
 
     // metadata
-    if( domain.rank==0 ) {
+    if (domain.rank == 0)
+    {
         std::ofstream fid("output.bov");
         fid << "TIME: 0.0" << std::endl;
         fid << "DATA_FILE: output.bin" << std::endl;
-        fid << "DATA_SIZE: " << options.nx << " " << options.ny << " 1" << std::endl;;
+        fid << "DATA_SIZE: " << options.nx << " " << options.ny << " 1" << std::endl;
+        ;
         fid << "DATA_FORMAT: DOUBLE" << std::endl;
         fid << "VARIABLE: phi" << std::endl;
         fid << "DATA_ENDIAN: LITTLE" << std::endl;
         fid << "CENTERING: nodal" << std::endl;
-        fid << "BRICK_SIZE: 1.0 " << (options.ny-1)*options.dx << " 1.0" << std::endl;
+        fid << "BRICK_SIZE: 1.0 " << (options.ny - 1) * options.dx << " 1.0" << std::endl;
     }
 
     // print table sumarizing results
-    if(domain.rank == 0) {
+    if (domain.rank == 0)
+    {
         std::cout << "--------------------------------------------------------------------------------"
                   << std::endl;
         std::cout << "simulation took " << timespent << " seconds" << std::endl;
         std::cout << int(iters_cg) << " conjugate gradient iterations, at rate of "
-                  << float(iters_cg)/timespent << " iters/second" << std::endl;
+                  << float(iters_cg) / timespent << " iters/second" << std::endl;
         std::cout << iters_newton << " newton iterations" << std::endl;
         std::cout << "--------------------------------------------------------------------------------"
                   << std::endl;
     }
 
-    if(domain.rank==0)
+    if (domain.rank == 0)
         std::cout << "Goodbye!" << std::endl;
 
-    //TODO: Finalize MPI
+    // Finalize MPI
+    MPI_Finalize();
 
     return 0;
 }
-
